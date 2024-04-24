8504d551586665cfebc487bbd6490cfe64626d91
JodieH <jodieherrington@hotmail.co.uk>
2024-04-24 10:09:47 +0100

    added playercontroller

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -3,3 +3,30 @@
 
 #include "Player/AuraPlayerController.h"
 
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// check Context exists before using
+	// if fails, game will crash, unlike with if check
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = UlocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer);
+	check(Subsystem);
+
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
